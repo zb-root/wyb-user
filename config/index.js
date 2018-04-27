f6cba8afa1c6fc7fb5f807f5679d373213c6b19b
jacsion <179407668@qq.com>
2018-04-27 13:15:23 +0800

    调整查询用户接口

--- config/index.js
+++ config/index.js
@@ -1,46 +1,46 @@
 require('log4js').configure(require('path').join(__dirname, 'log4js.json'))
 var config = {
-    development: {
-        port: 3000,
-        lng: 'zh_CN',
+  development: {
+    port: 3000,
+    lng: 'zh_CN',
 
-        // config: 'http://api.h5.jamma.cn/config', // 可选 如果不填，必须设置sdk
-        sso: 'http://api.h5.jamma.cn/sso', // 可选 如果不填，必须设置sdk
-        acl: 'http://api.h5.jamma.cn/acl',
+    // config: 'http://api.h5.jamma.cn/config', // 可选 如果不填，必须设置sdk
+    // sso: 'http://api.h5.jamma.cn/sso', // 可选 如果不填，必须设置sdk
+    // acl: 'http://api.h5.jamma.cn/acl',
 
-        // db: 'mongodb://localhost/main',
-        db: 'mongodb://root:123@api.h5.jamma.cn/main?authSource=admin',
-        avatarDir: process.cwd() + '/../uploads',
-        avatarPrefix: '/avatar',
-        mqtt: 'mqtt://root:123@api.h5.jamma.cn',
-        gateway: 'http://api.h5.jamma.cn',
-        modules: {
-            // 'jm-server-config': {},
-            'jm-server-sso': {},
-            'jm-server-acl': {},
-            'user': {
-                module: process.cwd() + '/lib'
-            },
-            'jm-user-mqtt': {}
-        }
-    },
-    production: {
-        port: 3000,
-        lng: 'zh_CN',
-        db: 'mongodb://mongo.db/user',
-        gateway: 'http://gateway.app',
-        avatarDir: process.cwd() + '/../uploads',
-        avatarPrefix: '/avatar',
-        tokenExpire: 3600,
-        disableVerifyCode: false,
-        disableAutoUid: false,
-        modules: {
-            'user': {
-                module: process.cwd() + '/lib'
-            },
-            'jm-user-mqtt': {}
-        }
+    // db: 'mongodb://localhost/main',
+    db: 'mongodb://root:123@api.h5.jamma.cn/wyb?authSource=admin',
+    avatarDir: process.cwd() + '/../uploads',
+    avatarPrefix: '/avatar',
+    mqtt: 'mqtt://root:123@api.h5.jamma.cn',
+    gateway: 'http://api.wyb.jamma.cn:81',
+    modules: {
+      // 'jm-server-config': {},
+      // 'jm-server-sso': {},
+      // 'jm-server-acl': {},
+      'user': {
+        module: process.cwd() + '/lib'
+      },
+      'jm-user-mqtt': {}
     }
+  },
+  production: {
+    port: 80,
+    lng: 'zh_CN',
+    db: 'mongodb://mongo.db/user',
+    gateway: 'http://gateway.app',
+    avatarDir: process.cwd() + '/../uploads',
+    avatarPrefix: '/avatar',
+    tokenExpire: 3600,
+    disableVerifyCode: false,
+    disableAutoUid: false,
+    modules: {
+      'user': {
+        module: process.cwd() + '/lib'
+      },
+      'jm-user-mqtt': {}
+    }
+  }
 }
 
 var env = process.env.NODE_ENV || 'development'
